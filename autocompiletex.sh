69b74aff1142a9719e936628605689f0f46d5bb0
simuns <sih@synack.fo>
2022-01-27 21:58:11 +0100

    added evince and code feature

--- autocompiletex.sh
+++ autocompiletex.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#User input section
 echo "-----------------------------------------"
 tput setaf 1; echo "Files to chose from:";tput sgr0
 ls *.tex
@@ -6,6 +7,22 @@ echo "-----------------------------------------"
 tput setaf 2;echo -n "input file to compile: ";tput sgr0
 read texfile
 pdflatex $texfile
+
+
+#Open PDF in evince
+read -p "Open PDF in evince ? (y/n)?" CONT
+if [ "$CONT" = "y" ]; then
+  filewoext=`echo "$texfile" | cut -d'.' -f1`
+  evince $filewoext.pdf&
+fi
+#Open library in code
+read -p "Open library in code? (y/n)?" CONT
+if [ "$CONT" = "y" ]; then
+  code .;
+fi
+
+
+#Auto compile feature
 checksum1=$(md5sum $texfile | cut -d " " -f1)
 checksum2=$checksum1
 echo "$checksum1 = $checksum2"
